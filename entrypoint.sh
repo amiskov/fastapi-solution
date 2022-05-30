7a141df55774620916a0f9aaf3de57b2a302d2f4
Vitaliy Rakitin <vitaliyrakitin@ya.ru>
2022-05-30 08:58:01 +0300

    Gunicorn added. (#20)
    Co-authored-by: Ракитин Виталий Павлович <Rakitin.V.Pa@sberbank.ru>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Starting FastAPI server..."
-uvicorn main:app --port 8000 --host 0.0.0.0 --app-dir .
+echo "Starting FastAPI server with workers count $GUNICORN_WORKERS..."
+gunicorn --workers="$GUNICORN_WORKERS" --bind="0.0.0.0:8000" --worker-class=uvicorn.workers.UvicornWorker main:app
