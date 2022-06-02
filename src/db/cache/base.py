94b5dedb8aa0a526e26432c10897a3b61a63c067
Andrey Miskov <amiskov@gmail.com>
2022-06-02 14:21:58 +0300

    Clarify docs

--- src/db/cache/base.py
+++ src/db/cache/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Callable, Optional
 
 from pydantic import BaseModel
 
@@ -8,10 +8,18 @@ class BaseCache(ABC):
     @abstractmethod
     async def get_entity_from_cache_or_db(
             self,
-            **kwargs
+            get_entity_from_db_fn: Callable,
+            entity_id: str
     ) -> Optional[BaseModel]:
-        pass
+        """Возвращает сущность из кэша. Если её в кэше нет, ходит за ней
+        в источник данных с помощью `get_entity_from_db_fn` и `entity_id`."""
 
     @abstractmethod
-    async def get_list_from_cache_or_db(self, **kwargs) -> list[BaseModel]:
-        pass
+    async def get_list_from_cache_or_db(
+            self,
+            get_list_from_db_fn: Callable,
+            **kwargs
+    ) -> list[BaseModel]:
+        """Возвращает список сущностей из кэша. Если их в кэше нет, ходит за
+        ними в источник данных с помощью `get_list_from_db_fn` и именованных
+        аргументов `kwargs`."""
