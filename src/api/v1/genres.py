9a4a51eb89c36f76cc0c5fed1f1e8228cca4fabb
Andrey Miskov <amiskov@gmail.com>
2022-06-10 12:15:27 +0300

    Add PagingParams for routes

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -1,8 +1,9 @@
 """API жанров."""
 from typing import Optional
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends
 
+from api.v1.paging_params import PagingParams
 from errors import GenreNotFoundException, MissedQueryParameterException
 from models.genre import GenreAPIResponse
 from services.genre import GenresService, get_genres_service
@@ -13,22 +14,21 @@ router = APIRouter()
 @router.get(
     '/',
     response_model=list[GenreAPIResponse],
-    summary="Список жанров.",
-    description="Полный список всех персон.",
-    response_description="Список всех персон.",
+    summary='Список жанров.',
+    description='Полный список всех персон.',
+    response_description='Список всех персон.',
     tags=['Список всех элементов'],
 )
 async def genres_list(
         genre_service: GenresService = Depends(get_genres_service),
         sort: Optional[str] = '-id',
-        page_size: int = Query(50, alias='page[size]', ge=1),
-        page_number: int = Query(1, alias='page[number]', ge=1),
+        paging_params: PagingParams = Depends(),
 ) -> list[GenreAPIResponse]:
     """Возвращает список жанров для отправки по API, соответствующий критериям фильтрации."""
     genres = await genre_service.get_list(
         sort=sort,
-        page_size=page_size,
-        page_number=page_number,
+        page_size=paging_params.page_size,
+        page_number=paging_params.page_number,
     )
     return [GenreAPIResponse(**item.dict()) for item in genres]
 
@@ -36,16 +36,15 @@ async def genres_list(
 @router.get(
     '/search',
     response_model=list[GenreAPIResponse],
-    summary="Поиск по жанрам.",
-    description="Полнотекстовый поиск по жанрам.",
-    response_description="Список всех жанров.",
+    summary='Поиск по жанрам.',
+    description='Полнотекстовый поиск по жанрам.',
+    response_description='Список всех жанров.',
     tags=['Полнотекстовый поиск'],
 )
 async def genres_search(
         genre_service: GenresService = Depends(get_genres_service),
         query: str = None,
-        page_size: int = Query(50, alias='page[size]', ge=1),
-        page_number: int = Query(1, alias='page[number]', ge=1),
+        paging_params: PagingParams = Depends(),
 ) -> list[GenreAPIResponse]:
     """Возвращает список жанров для отправки по API, соответствующий критериям поиска."""
     if not query:
@@ -53,8 +52,8 @@ async def genres_search(
 
     search_result = await genre_service.get_search_result(
         query=query,
-        page_size=page_size,
-        page_number=page_number,
+        page_size=paging_params.page_size,
+        page_number=paging_params.page_number,
     )
 
     return [GenreAPIResponse(**item.dict()) for item in search_result]
@@ -63,9 +62,9 @@ async def genres_search(
 @router.get(
     '/{genre_id}',
     response_model=GenreAPIResponse,
-    summary="Детализация жанра.",
-    description="Детализация жанра по id.",
-    response_description="Подробная информация о жанре (наименование и описание).",
+    summary='Детализация жанра.',
+    description='Детализация жанра по id.',
+    response_description='Подробная информация о жанре (наименование и описание).',
     tags=['Получение даннх по id'],
 )
 async def genre_details(
